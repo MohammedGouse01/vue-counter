84d5425b807d6bc218f61233bf1873d4575323b5
MohammedGouse01 <gousetaj001@gmail.com>
2024-12-03 08:12:20 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import CounterComponent from './components/CounterComponent.vue';
+</script>
+
+<template>
+  <CounterComponent />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/CounterComponent.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    <h3>
+        Current Count : {{ currentCount }}
+    </h3>
+    <button @click="incrementFunction" id="IncrementButton">Increment</button>
+    <button @click="decrementFunction" id="decrementButton">Decrement</button>
+</div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const currentCount=ref<number>(0)
+
+const incrementFunction=()=>{
+    currentCount.value++
+}
+const decrementFunction=()=>{
+    if(currentCount.value>0)currentCount.value--
+}
+</script>
